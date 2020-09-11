67ae1241c110a9a686fe0d7e505f68a9adfc905f
Rafael S. Campos <rafael.schubert.campos@gmail.com>
2020-09-11 11:51:13 -0300

    Splitting code
    Moving lcs function into a header file.

--- lcsAlgorithm/main.cpp
+++ lcsAlgorithm/main.cpp
@@ -1,32 +1,6 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <string>
-
-
-template<class TLeft, class TRight>
-std::size_t lcs(TLeft const& left, TRight const& right)
-{
-    std::vector<std::size_t> scores(size(right) + 1, 0);
-    std::size_t              scoreIndex = 1;
-
-    for (auto const& leftValue : left)
-    {
-        for (auto const& rightValue : right)
-        {
-            scores[scoreIndex] = std::max(scores[scoreIndex - 1], scores[scoreIndex]);
-
-            if (leftValue == rightValue)
-                ++scores[scoreIndex];
-
-            ++scoreIndex;
-        }
-
-        scoreIndex = 1;
-    }
-
-    return scores.back();
-}
+#include "lcs.h"
 
 
 int main()
