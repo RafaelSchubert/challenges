310f72b0152e48b0cda85178324178965110061e
Rafael S. Campos <rafael.schubert.campos@gmail.com>
2020-10-15 15:12:52 -0300

    Implementation of the scope_trigger class.
    Initial implementation of the scope_trigger class.
    The purpose of this class is to provide a simple way to trigger actions when exiting a scope, such as a function scope.

--- cpp/scopeTrigger/scope_trigger.hpp
+++ cpp/scopeTrigger/scope_trigger.hpp
@@ -1,9 +1,60 @@
 #ifndef SCOPE_TRIGGER_H
 #define SCOPE_TRIGGER_H
 
+#include <functional>
 
-class scope_trigger
+class scope_trigger final
 {
+public:
+    scope_trigger() noexcept = default;
+
+    template<class TAction>
+    scope_trigger(TAction&& action)
+    {
+        set_action(std::forward<TAction>(action));
+    }
+
+    scope_trigger(scope_trigger&& other) noexcept
+    {
+        *this = std::move(other);
+    }
+
+    ~scope_trigger()
+    {
+        if (enabled())
+            m_action();
+    }
+
+    scope_trigger& operator=(scope_trigger&& other) noexcept
+    {
+        m_action = std::move(other.m_action);
+
+        return *this;
+    }
+
+    bool enabled() const
+    {
+        return bool{ m_action };
+    }
+
+    template<class TAction>
+    void set_action(TAction&& action)
+    {
+        m_action = std::forward<TAction>(action);
+    }
+
+    void disable()
+    {
+        set_action(nullptr);
+    }
+
+private:
+    scope_trigger(scope_trigger const&) = delete;
+
+    scope_trigger& operator=(scope_trigger const&) = delete;
+
+private:
+    std::function<void(void)> m_action;
 };
 
 
