a233b93f77c4314ccb4daa9752446512ab479e9c
Rafael S. Campos <rafael.schubert.campos@gmail.com>
2020-10-14 21:32:31 -0300

    Moving C# challenges from csharp-challenges to challenges/csharp.

---
+++ csharp/circularRotation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the circularArrayRotation function below.
+
+    static int[] circularArrayRotation(int[] a, int k, int[] queries)
+    {
+        var query_result = new int[queries.Length];
+
+        for (int idx = 0; idx < queries.Length; ++idx)
+        {
+            query_result[idx] = a[(queries[idx] - k % a.Length + a.Length) % a.Length];
+        }
+
+        return query_result;
+    }
+
+    static void Main(string[] args)
+    {
+        string[] nkq = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nkq[0]);
+
+        int k = Convert.ToInt32(nkq[1]);
+
+        int q = Convert.ToInt32(nkq[2]);
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+
+        int[] queries = new int[q];
+
+        for (int i = 0; i < q; i++)
+        {
+            int queriesItem = Convert.ToInt32(Console.ReadLine());
+            queries[i] = queriesItem;
+        }
+
+        int[] result = circularArrayRotation(a, k, queries);
+
+        Console.WriteLine(string.Join("\n", result));
+    }
+}
---
+++ csharp/pickingNumbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    // Complete the 'pickingNumbers' function below.
+
+    public static int pickingNumbers(List<int> a)
+    {
+        a.Sort();
+
+        int biggest_size = 0;
+        int search_start_index = 0;
+
+        do {
+            int sequence_end_index = a.FindIndex(search_start_index + 1, x => (x - a[search_start_index]) > 1);
+
+            int sequence_size = ((sequence_end_index < 0) ? a.Count : sequence_end_index) - search_start_index;
+
+            if (sequence_size > biggest_size)
+                biggest_size = sequence_size;
+
+            search_start_index = a.FindIndex(search_start_index + 1, x => x > a[search_start_index]);
+        } while (search_start_index >= 0);
+
+        return biggest_size;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.pickingNumbers(a);
+
+        Console.WriteLine(result);
+    }
+}
